62d051b0fa7b297c5bbe7f60d50fb3d686b13e0c
Elliot Jackson <13633636+elliotmjackson@users.noreply.github.com>
2024-04-03 13:33:52 -0400

    resolve to AnyModule protocol

--- ios/IntegrityModule.swift
+++ ios/IntegrityModule.swift
@@ -1,18 +1,22 @@
-import CryptoKit
-import DeviceCheck
 import ExpoModulesCore
 
 @available(iOS 13.4, *)
-public class IntegrityModule: AnyModule {
-
+public class IntegrityModule: Module {
     public func definition() -> ModuleDefinition {
         Name("Integrity")
 
-        // Add minimal implementation to conform to AnyModule protocol
-        AsyncFunction("placeholder") { () async -> Void in
-            // TODO: Replace with actual implementation compatible with the required OS version
-        }
-    }
+        Function("isSupported") {}
 
-    // TODO: Implement the module's functionality compatible with the required OS version
+        AsyncFunction("generateKey") {}
+
+        AsyncFunction("attestKey") { (
+            keyIdentifier: String,
+            challenge: String
+        ) async throws -> String in }
+
+        AsyncFunction("generateAssertion") { (
+            keyIdentifier: String,
+            requestJSON: String
+        ) async throws -> String in }
+    }
 }
