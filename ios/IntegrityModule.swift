9467c5820956307117d791a870859d9aea141088
Elliot Jackson <13633636+elliotmjackson@users.noreply.github.com>
2024-04-03 13:25:43 -0400

    resolve to AnyModule protocol

--- ios/IntegrityModule.swift
+++ ios/IntegrityModule.swift
@@ -3,6 +3,16 @@ import DeviceCheck
 import ExpoModulesCore
 
 @available(iOS 13.4, *)
-public class IntegrityModule: Module {
+public class IntegrityModule: AnyModule {
+
+    public func definition() -> ModuleDefinition {
+        Name("Integrity")
+
+        // Add minimal implementation to conform to AnyModule protocol
+        AsyncFunction("placeholder") { () async -> Void in
+            // TODO: Replace with actual implementation compatible with the required OS version
+        }
+    }
+
     // TODO: Implement the module's functionality compatible with the required OS version
 }
